937bce6684ebe79b5e3926ab3deacc9d62e1c25c
Sritan <143901718+VS-310@users.noreply.github.com>
2024-08-01 08:25:19 +0530

    new

--- mcqbattle/mcqbattle/settings.py
+++ mcqbattle/mcqbattle/settings.py
@@ -143,9 +143,9 @@ REST_FRAMEWORK = {
 
 
 
-BARD_API_KEY= "key here. in my clipboard currently else google cloud dashboard"
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://127.0.0.1:8080"
-]
+#BARD_API_KEY= "key here. in my clipboard currently else google cloud dashboard"
+#CORS_ALLOWED_ORIGINS = [
+#    "http://localhost:5173",
+#    "http://127.0.0.1:8080"
+#]
 
